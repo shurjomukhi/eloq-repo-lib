929e89f3f84e2bb02ea4320e5633c7c1fcd307a0
Imtiaz Rahi <imtiaz.rahi@gmail.com>
2023-01-04 19:59:12 +0600

    Initial commit w/ interface, impl copied from engine

---
+++ EloquentBaseRepository.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Base implementation of repository interface to accommodate Eloquent models.
+ *
+ * @package App\Interfaces
+ * @author Imtiaz Rahi
+ * @since 2022-11-06
+ * @see https://dev.to/carlomigueldy/getting-started-with-repository-pattern-in-laravel-using-inheritance-and-dependency-injection-2ohe
+ */
+class EloquentBaseRepository implements EloquentRepositoryInterface
+{
+    /** Name of the database model */
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
+
+    public function findById($recordId, array $columns = ['*'], array $relations = [], array $appends = []): ?Model
+    {
+        return $this->model->select($columns)->with($relations)->findOrFail($recordId)->append($appends);
+    }
+
+    public function create(array $payload): ?Model
+    {
+        if ($this->fixPayload($payload) == null) return false;
+        $model = $this->model->create($payload);
+        return $model->fresh();
+    }
+
+    public function update($recordId, array $payload): bool
+    {
+        if ($this->fixPayload($payload) == null) return false;
+        $model = $this->findById($recordId);
+        return $model->update($payload);
+    }
+
+    /**
+     * Eloquent expects payload for create or update as array data.
+     *
+     * @param $data
+     * @return array
+     */
+    private function fixPayload($data): array {
+        switch (gettype($data)) {
+            case "array": return $data;
+            break;
+            case "object": return $data->attributesToArray();
+            break;
+        }
+        return false;
+    }
+
+    /**
+     * Archive (soft delete) individual record.
+     *
+     * @param int $recordId
+     * @return bool
+     */
+    public function archive($recordId): bool
+    {
+        $model = $this->findById($recordId);
+        //TODO update that record by setting archive field to true;
+        return true;
+    }
+
+    public function deleteById($recordId): bool
+    {
+        return $this->findById($recordId)->delete();
+    }
+
+    public function restoreById($recordId): bool
+    {
+        return $this->findTrashedById($recordId)->restore();
+    }
+
+    public function permanentlyDeleteById($recordId): bool
+    {
+        return $this->findTrashedById($recordId)->forceDelete();
+    }
+
+    public function allTrashed(): Collection
+    {
+        return $this->model->onlyTrashed()->get();
+    }
+
+    public function findTrashedById($recordId): ?Model
+    {
+        return $this->model->withTrashed()->findOrFail($recordId);
+    }
+
+    public function save(array $payload): ?Model
+    {
+        return $this->create($payload);
+    }
+
+    public function find($recordId, array $criteria = [], array $columns = ['*'], array $relations = [], array $appends = []): ?Collection
+    {
+        return $this->model->with($relations)->where($criteria)->get($columns);
+    }
+
+}
---
+++ EloquentRepositoryInterface.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Repository interface to accommodate Eloquent models.
+ * TODO need method with orderBy option
+ *
+ * @package App\Interfaces
+ * @author Imtiaz Rahi
+ * @since 2022-11-06
+ * @see https://dev.to/carlomigueldy/getting-started-with-repository-pattern-in-laravel-using-inheritance-and-dependency-injection-2ohe
+ */
+interface EloquentRepositoryInterface
+{
+
+    /**
+     * Get all models.
+     *
+     * @param array $columns Columns of a DB table
+     * @param array $relations
+     * @return Collection
+     */
+    public function all(array $columns = ['*'], array $relations = []): Collection;
+
+    /**
+     * Find model by record id.
+     *
+     * @param mixed $recordId Record id
+     * @param array $columns
+     * @param array $relations
+     * @param array $appends
+     * @return Model|null
+     */
+    public function findById($recordId, array $columns = ['*'], array $relations = [], array $appends = []): ?Model;
+
+    /**
+     * Create a model with payload.
+     *
+     * @param array $payload
+     * @return Model|null
+     */
+    public function create(array $payload): ?Model;
+
+    /**
+     * Update existing model.
+     *
+     * @param mixed $recordId
+     * @param array $payload
+     * @return bool
+     */
+    public function update($recordId, array $payload): bool;
+
+    /**
+     * Archive selected record (0row) by it's id.
+     *
+     * @param mixed $recordId
+     * @return bool
+     */
+    public function archive($recordId): bool;
+
+    /**
+     * Delete model by id.
+     *
+     * @param mixed $recordId
+     * @return bool
+     */
+    public function deleteById($recordId): bool;
+
+    public function restoreById($recordId): bool;
+
+    public function permanentlyDeleteById($recordId): bool;
+
+    /**
+     * Get all trashed collections.
+     *
+     * @return Collection
+     */
+    public function allTrashed(): Collection;
+
+    /**
+     * Find trashed model by id.
+     *
+     * @param mixed $recordId
+     * @return Model|null
+     */
+    public function findTrashedById($recordId): ?Model;
+
+    /**
+     * @param array $payload
+     * @return Model|null
+     */
+    public function save(array $payload): ?Model;
+
+    /**
+     * Find records with a single where clause.
+     *
+     * @param mixed $recordId Record id
+     * @param array $criteria Eloquent Where clause
+     * @param array $columns Column names (optional)
+     * @param array $relations Table relations (optional)
+     * @param array $appends
+     * @return Collection|null
+     */
+    public function find($recordId, array $criteria = [], array $columns = ['*'], array $relations = [], array $appends = []): ?Collection;
+
+}
